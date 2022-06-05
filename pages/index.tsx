393ac23859cd87b290187b5edde439edf2d29496
Surer Sukrucan <sukrucan.surer@sisal.it>
2022-06-05 22:44:58 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Disney +</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Disney+</h1>
+    </div>
+  );
+};
+
+export default Home;
